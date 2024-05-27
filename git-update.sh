e98abfde0f8f857029989d8d6c18acee866d7866
Blackh00d <blackh00d@github.com>
2024-05-27 11:24:04 -0500

    Update

--- git-update.sh
+++ git-update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Script to quickly push updates to a git repo.
-
+echo "Configuring repo email"
 git config --global user.email "blackh00d@github.com"
+echo "Configuring repo username"
 git config --global user.name "Blackh00d"
+echo "Adding recent changes"
 git add .
+echo "Commiting changes with comment"
 git commit -m "Update"
+echo "Pushing updates to repo"
 git push origin main
--- gomod-update.sh
+++ gomod-update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Script to quickly reinitialize a Go module and rebuild from scratch
-
+echo "Removing old Go mod file"
 rm go.*
+echo "Removing old binary...if present"
 rm $gomod
+echo "Reinitializing Go module"
 go mod init $gomod
+echo "Tidying up"
 go mod tidy
+echo "Building new binary"
 go build -o $gomod
